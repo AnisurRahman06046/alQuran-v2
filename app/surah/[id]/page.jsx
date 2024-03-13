d4f8770258b929b5ae91906e9aaf16d491939197
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-03-14 01:13:39 +0600

    back button is added

--- app/surah/[id]/page.jsx
+++ app/surah/[id]/page.jsx
@@ -1,13 +1,23 @@
 import getSingleSurah from "@/lib/getSingleSurah";
+import Link from "next/link";
 import React from "react";
 
 async function SingleSurah({ params }) {
   const { id } = params;
   const singleSurah = await getSingleSurah(id);
-  return <div>SingleSurah {id}
-
-  <h1>Surah : {singleSurah.name.short} {singleSurah.name.transliteration.en}</h1>
-  </div>;
+  return (
+    <div>
+      SingleSurah {id}
+      <h1>
+        Surah : {singleSurah.name.short} {singleSurah.name.transliteration.en}
+      </h1>
+      <Link href="/">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Back to previous
+        </button>
+      </Link>
+    </div>
+  );
 }
 
 export default SingleSurah;
--- components/Surah/Surah.jsx
+++ components/Surah/Surah.jsx
@@ -10,7 +10,9 @@ async function Surah() {
       <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {surahs.map((surah, index) => (
-            <Link key={surah.number} href={`/surah/${surah.number}`}><Card key={index} surah={surah} /></Link>
+            <Link key={surah.number} href={`/surah/${surah.number}`}>
+              <Card key={index} surah={surah} />
+            </Link>
             // <Card key={index} surah={surah} /> // Use the Card component passing the surah as a prop
           ))}
         </div>
